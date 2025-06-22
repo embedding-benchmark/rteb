64b018b03058b14a8e04bd3990e1c322ca45cd5f
fzowl <160063452+fzowl@users.noreply.github.com>
2025-06-22 07:42:24 +0200

    Qwen and google models (#7)

--- ebr/models/sentence_transformers.py
+++ ebr/models/sentence_transformers.py
@@ -27,6 +27,46 @@ class SentenceTransformersEmbeddingModel(EmbeddingModel):
         return f"{self.model_name_prefix}__{self._model_meta._id}"
 
 
+class E5EmbeddingModel(SentenceTransformersEmbeddingModel):
+    @property
+    def model_name_prefix(self) -> str:
+        return "intfloat"
+
+
+class JinaEmbeddingModel(SentenceTransformersEmbeddingModel):
+    @property
+    def model_name_prefix(self) -> str:
+        return "jinaai"
+
+
+class QwenEmbeddingModel(SentenceTransformersEmbeddingModel):
+    @property
+    def model_name_prefix(self) -> str:
+        return "Qwen"
+
+
+qwen3_embedding_8B = ModelMeta(
+    loader=QwenEmbeddingModel,
+    model_name="Qwen3-Embedding-8B",
+    embd_dtype="float32",
+    embd_dim=4096,
+    max_tokens=32768,
+    num_params=8_000_000_000,
+    similarity="cosine",
+    reference="https://huggingface.co/Qwen/Qwen3-Embedding-8B"
+)
+
+
+e5_mistral_7b_instruct = ModelMeta(
+    loader=E5EmbeddingModel,
+    model_name="e5-mistral-7b-instruct",
+    embd_dtype="float32",
+    embd_dim=4096,
+    similarity="cosine",
+    reference="https://huggingface.co/intfloat/e5-mistral-7b-instruct"
+)
+
+
 all_MiniLM_L6_v2 = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
     model_name="all-MiniLM-L6-v2",
@@ -38,10 +78,10 @@ all_MiniLM_L6_v2 = ModelMeta(
     reference="https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2"
 )
 
-"""
+
 all_MiniLM_L12_v2 = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
-    model_name="sentence-transformers/all-MiniLM-L12-v2",
+    model_name="all-MiniLM-L12-v2",
     embd_dtype="float32",
     embd_dim=384,
     num_params=33_400_000,
@@ -53,7 +93,7 @@ all_MiniLM_L12_v2 = ModelMeta(
 
 labse = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
-    model_name="sentence-transformers/LaBSE",
+    model_name="LaBSE",
     embd_dtype="float32",
     embd_dim=768,
     num_params=471_000_000,
@@ -65,7 +105,7 @@ labse = ModelMeta(
 
 multi_qa_MiniLM_L6_cos_v1 = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
-    model_name="sentence-transformer/multi-qa-MiniLM-L6-cos-v1",
+    model_name="multi-qa-MiniLM-L6-cos-v1",
     embd_dtype="float32",
     embd_dim=384,
     num_params=22_700_000,
@@ -77,7 +117,7 @@ multi_qa_MiniLM_L6_cos_v1 = ModelMeta(
 
 all_mpnet_base_v2 = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
-    model_name="sentence-transformers/all-mpnet-base-v2",
+    model_name="all-mpnet-base-v2",
     embd_dtype="float32",
     embd_dim=768,
     num_params=109_000_000,
@@ -89,7 +129,7 @@ all_mpnet_base_v2 = ModelMeta(
 
 jina_embeddings_v2_base_en = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
-    model_name="jinaai/jina-embeddings-v2-base-en",
+    model_name="jina-embeddings-v2-base-en",
     embd_dtype="float32",
     embd_dim=768,
     num_params=137_000_000,
@@ -101,7 +141,7 @@ jina_embeddings_v2_base_en = ModelMeta(
 
 jina_embeddings_v2_small_en = ModelMeta(
     loader=SentenceTransformersEmbeddingModel,
-    model_name="jinaai/jina-embeddings-v2-small-en",
+    model_name="jina-embeddings-v2-small-en",
     embd_dtype="float32",
     embd_dim=512,
     num_params=32_700_000,
@@ -109,5 +149,5 @@ jina_embeddings_v2_small_en = ModelMeta(
     similarity="cosine",
     reference="https://huggingface.co/jinaai/jina-embeddings-v2-small-en"
 )
-"""
+
 
